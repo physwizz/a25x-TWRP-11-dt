256c7e1a690fbad27eb0aa566db451d3d90164ce
physwizz <pollockg86@gmail.com>
2024-03-24 18:13:29 +1100

    first

---
+++ twrp_a25x.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from a25x device
+$(call inherit-product, device/samsung/a25x/device.mk)
+
+PRODUCT_DEVICE := a25x
+PRODUCT_NAME := twrp_a25x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A256E
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a25xdxx-user 12 SP1A.210812.016 A256EXXU1AXB4 release-keys"
+
+BUILD_FINGERPRINT := samsung/a25xdxx/a25x:12/SP1A.210812.016/A256EXXU1AXB4:user/release-keys
