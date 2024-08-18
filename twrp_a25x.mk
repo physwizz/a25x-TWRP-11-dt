10b66d872de2e2730f7f0ba35945320d23780302
physwizz <pollockg86@gmail.com>
2024-08-18 13:47:02 +1000

    fix 2

--- twrp_a25x.mk
+++ twrp_a25x.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a25x device
 $(call inherit-product, device/samsung/a25x/device.mk)
